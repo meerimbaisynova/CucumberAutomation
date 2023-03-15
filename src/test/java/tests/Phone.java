5ae721fe5b5c0141cedf413e1473599cf3e98eb7
meerimbaisynova <meerim.baisynova@gmail.com>
2023-03-15 12:38:31 -0500

    initial commit

---
+++ src/test/java/tests/Phone.java
@@ -0,0 +1,12 @@
+package tests;
+
+import utilities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+    }
+
+}
+
